4a94b320dc54fd8e43ff311175519056e06b8577
GenerativeGig <generativegig@gmail.com>
2022-10-14 00:02:20 +0200

    Style create sesion

--- frontend/src/views/CreateSession/CreateSession.tsx
+++ frontend/src/views/CreateSession/CreateSession.tsx
@@ -1,18 +1,22 @@
 export function CreateSessionView() {
   return (
     <section className="h-full w-full flex flex-col items-center">
-      <div className="m-2 p-4 w-full max-w-[768px] bg-slate-800">
-        <h1 className="text-2xl font-medium">Create your Session</h1>
+      <div className="m-2  p-2 w-full max-w-[768px] bg-slate-800">
+        <h1 className="text-2xl font-medium mx-8 mt-6 mb-4">
+          Create your Session
+        </h1>
         <form className="p-2 flex flex-col items-start">
           <div>
             <label htmlFor="title">Title</label>
             <input name="title" type="text" placeholder="Title" required />
           </div>
-          <div>
-            <label htmlFor="description">Description</label>
-            <input
+          <div className="flex">
+            <label className="self-start" htmlFor="description">
+              Description
+            </label>
+            <textarea
+              className="w-[500px] h-[200px]"
               name="description"
-              type="text"
               placeholder="Description"
               required
             />
@@ -21,29 +25,32 @@ export function CreateSessionView() {
             <label htmlFor="date">Date</label>
             <input name="date" type="date" required />
           </div>
-          <div>
-            <label htmlFor="time">Time</label>
-            <input name="time" type="time" required />
-            <span> - </span>
-            <input name="time" type="time" required />
+          <div className="w-full flex justify-between">
+            <div>
+              <div>
+                <label htmlFor="time">Time</label>
+                <input name="time" type="time" required />
+                <span> - </span>
+                <input name="time" type="time" required />
+              </div>
+              <div>
+                <label htmlFor="participantLimit">Participant limit</label>
+                <input
+                  className="w-16"
+                  name="participantLimit"
+                  type="number"
+                  min={0}
+                  defaultValue={8}
+                  required
+                />
+              </div>
+            </div>
+            <div className="self-end">
+              <button type="submit" className="bg-pink-600 hover:bg-pink-500">
+                Create Session
+              </button>
+            </div>
           </div>
-          <div>
-            <label htmlFor="participantLimit">Participant limit</label>
-            <input
-              name="participantLimit"
-              type="number"
-              min={0}
-              defaultValue={5}
-              required
-            />
-          </div>
-
-          <button
-            type="submit"
-            className="bg-pink-600 hover:bg-pink-500 self-end"
-          >
-            Create Session
-          </button>
         </form>
       </div>
     </section>
