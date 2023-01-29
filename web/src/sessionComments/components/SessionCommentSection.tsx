e2955e36a0e1c3b62c8dc39f6788d23de7ed435d
GenerativeGig <generativegig@gmail.com>
2023-01-29 14:51:26 +0100

    Use data provider for session comments

--- web/src/sessionComments/components/SessionCommentSection.tsx
+++ web/src/sessionComments/components/SessionCommentSection.tsx
@@ -1,6 +1,5 @@
 import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/solid";
-import { FailedLoadingData } from "../../common/components/FailedLoadingData";
-import { Loading } from "../../common/components/Loading";
+import { DataProvider } from "../../common/components/DataProvider";
 import { Actor, useSessionCommentsQuery } from "../../generatedTypes";
 import { CreateSessionComment } from "./CreateSessionComment";
 import { SessionCommentCard } from "./SessionCommentCard";
@@ -14,41 +13,31 @@ export function SessionCommentSection({
   sessionId,
   me,
 }: SessionCommentSectionProps) {
-  const [{ data, fetching }] = useSessionCommentsQuery({
-    variables: { sessionId },
-  });
-
-  if (!data && fetching) {
-    return <Loading />;
-  }
-
-  if (!data && !fetching) {
-    return <FailedLoadingData />;
-  }
-
-  if (data && !fetching) {
-    return (
-      <div className="p-4 md:p-16">
-        <h2 className="p-5 text-center text-xl">
-          <ChatBubbleLeftRightIcon className="inline h-5 w-5" />
-          Comments
-        </h2>
-        <CreateSessionComment sessionId={sessionId} />
-        <ol className="flex w-full flex-col">
-          {data.sessionComments?.map((sessionComment) => (
-            <li
-              key={sessionComment.id}
-              className={`m-1 w-full rounded-md py-5 first:mt-2 last:mb-2`}
-            >
-              <SessionCommentCard sessionComment={sessionComment} me={me} />
-            </li>
-          ))}
-        </ol>
-      </div>
-    );
-  }
-  return <></>;
+  return (
+    <DataProvider useQuery={useSessionCommentsQuery} variables={{ sessionId }}>
+      {(data) => {
+        return (
+          <div className="p-4 md:p-16">
+            <h2 className="p-5 text-center text-xl">
+              <ChatBubbleLeftRightIcon className="inline h-5 w-5" />
+              Comments
+            </h2>
+            <CreateSessionComment sessionId={sessionId} />
+            <ol className="flex w-full flex-col">
+              {data.sessionComments?.map((sessionComment) => (
+                <li
+                  key={sessionComment.id}
+                  className={`m-1 w-full rounded-md py-5 first:mt-2 last:mb-2`}
+                >
+                  <SessionCommentCard sessionComment={sessionComment} me={me} />
+                </li>
+              ))}
+            </ol>
+          </div>
+        );
+      }}
+    </DataProvider>
+  );
 }
 
-// TODO: Reorganize code into domains: Actor (login etc.), Session (contains SessionComment?)
 // TODO: Proper error handling
